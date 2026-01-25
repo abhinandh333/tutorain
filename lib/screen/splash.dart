39587885268b78045d203ea99cfe14198b1f0588
Abhinandh R Nair <abhinandhrnair333@gmail.com>
2026-01-25 16:32:29 +0530

    UI update

--- lib/screen/splash.dart
+++ lib/screen/splash.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 
-
 class Splash extends StatefulWidget {
   const Splash({super.key});
 
@@ -9,24 +8,65 @@ class Splash extends StatefulWidget {
   State<Splash> createState() => _SplashState();
 }
 
-class _SplashState extends State<Splash> {
+class _SplashState extends State<Splash>
+    with SingleTickerProviderStateMixin {
+
+  late AnimationController _controller;
 
   @override
   void initState() {
     super.initState();
 
-    Timer(const Duration(seconds: 2), () {
-      Navigator.pushReplacementNamed(context, '/');
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 5),
+    )..repeat();
 
-  });
-    
+    Timer(const Duration(milliseconds: 1500), () {
+      Navigator.pushReplacementNamed(context, '/');
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 72, 126, 72),
       body: Center(
-        child: CircularProgressIndicator(),
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+
+            // ROTATING CIRCLE
+            RotationTransition(
+              turns: _controller,
+              child: SizedBox(
+                width: 140,
+                height: 140,
+                child: CircularProgressIndicator(
+                  strokeWidth: 4,
+                  valueColor: AlwaysStoppedAnimation(Colors.white),
+                ),
+              ),
+            ),
+
+            // CENTER "T"
+            const Text(
+              "T",
+              style: TextStyle(
+                fontSize: 64,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+                letterSpacing: 2,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
